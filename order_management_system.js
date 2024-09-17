4f68ce0d0466e4b357ada301d716cc3f3b721985
Je4n-A <jeanma6742@gmail.com>
2024-09-17 12:58:44 -0400

    "Initialize inventory with product objects"

---
+++ order_management_system.js
@@ -0,0 +1,29 @@
+//Create an inventory Array of Product Objects
+
+ let inventory = [
+    {
+        name: "coffee",
+        price: 2.50,
+        quantity: 10
+    },
+    {
+        name: "latte",
+        price: 3.50,
+        quantity: 20
+    },
+    {
+        name: "cappuccino",
+        price: 4.50,
+        quantity: 30
+    },
+    {
+        name: "espresso",
+        price: 1.50,
+        quantity: 40
+    }
+];
+
+
+
+    
+
